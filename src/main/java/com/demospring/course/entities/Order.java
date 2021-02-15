26ab5bed091be258462a33e022e26c3d6231bc73
Luciano Oliveira <lcoliveiraa@gmail.com>
2021-02-15 08:36:15 -0300

    OrderStatus enum

--- src/main/java/com/demospring/course/entities/Order.java
+++ src/main/java/com/demospring/course/entities/Order.java
@@ -11,6 +11,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import com.demospring.course.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonFormat.Shape;
 
@@ -25,6 +26,21 @@ public class Order implements Serializable {
 	@JsonFormat(shape = Shape.STRING,pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'",timezone = "GMT")
 	private Instant moment;
 
+	private Integer orderStatus;
+	
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(orderStatus);
+	}
+
+	public void setOrderStatus(OrderStatus orderStatus) {
+		if(orderStatus!=null)
+		this.orderStatus = orderStatus.getCode();
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
 	@ManyToOne
 	@JoinColumn(name = "client_id")
 	private User client;
@@ -33,9 +49,10 @@ public class Order implements Serializable {
 		// TODO Auto-generated constructor stub
 	}
 
-	public Order(Long id, Instant moment, User client) {
+	public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
 		this.id = id;
 		this.moment = moment;
+		setOrderStatus(orderStatus);
 		this.client = client;
 	}
 
