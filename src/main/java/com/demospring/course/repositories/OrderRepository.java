07e06ac16ff4097dd4a4d2c7541ede53d634ced5
Luciano Oliveira <lcoliveiraa@gmail.com>
2021-02-15 08:01:16 -0300

    Order, Instant, ISO 8601

---
+++ src/main/java/com/demospring/course/repositories/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.demospring.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.demospring.course.entities.Order;
+
+public interface OrderRepository extends JpaRepository<Order,Long> {
+
+}
