bf3795d9ba6e5f69b3590e749e9fecd69b3f8a39
Luciano Oliveira <lcoliveiraa@gmail.com>
2021-02-15 16:41:57 -0300

    Product

--- src/main/java/com/demospring/course/entities/Product.java
+++ src/main/java/com/demospring/course/entities/Product.java
@@ -12,37 +12,72 @@ import javax.persistence.Table;
 import javax.persistence.Transient;
 
 @Entity
-@Table(name = "tb_category")
-public class Category implements Serializable{
+@Table(name = "tb_product")
+public class Product implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
-	
+	private String description;
+	private Double price;
+	private String imgURL;
+
 	@Transient
-	private Set<Product> products = new HashSet<>();
+	private Set<Category> categories = new HashSet<>();
 	
-	public Category(Long id, String name) {
+	public Product() {
+	}
+
+	public Product(Long id, String name, String description, Double price, String imgURL) {
 		this.id = id;
 		this.name = name;
+		this.description = description;
+		this.price = price;
+		this.imgURL = imgURL;
 	}
-	public Category() {
-		// TODO Auto-generated constructor stub
-	}
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public String getImgURL() {
+		return imgURL;
+	}
+
+	public void setImgURL(String imgURL) {
+		this.imgURL = imgURL;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -50,6 +85,7 @@ public class Category implements Serializable{
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -58,7 +94,7 @@ public class Category implements Serializable{
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Category other = (Category) obj;
+		Product other = (Product) obj;
 		if (id == null) {
 			if (other.id != null)
 				return false;
@@ -66,9 +102,10 @@ public class Category implements Serializable{
 			return false;
 		return true;
 	}
-	public Set<Product> getProducts() {
-		return products;
+
+	public Set<Category> getCategories() {
+		return categories;
 	}
 	
-	
+
 }
---
+++ src/main/java/com/demospring/course/repositories/ProductRepository.java
@@ -0,0 +1,8 @@
+package com.demospring.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.demospring.course.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
