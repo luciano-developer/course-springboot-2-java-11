0970d939aa230c460361864948af36b6162e3bc0
Luciano Oliveira <lcoliveiraa@gmail.com>
2021-02-19 05:54:23 -0300

    Subtotal & Total methods

--- src/main/java/com/demospring/course/entities/Order.java
+++ src/main/java/com/demospring/course/entities/Order.java
@@ -39,10 +39,9 @@ public class Order implements Serializable {
 
 	@OneToMany(mappedBy = "id.order")
 	private Set<OrderItem> items = new HashSet<>();
-	
+
 	@OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
 	private Payment payment;
-	
 
 	public Order() {
 	}
@@ -90,12 +89,21 @@ public class Order implements Serializable {
 	public Payment getPayment() {
 		return payment;
 	}
-	
+
 	public void setPayment(Payment payment) {
 		this.payment = payment;
 	}
+
 	public Set<OrderItem> getItems() {
 		return items;
+	}
+
+	public Double getTotal() {
+		double sum = 0.0;
+		for (OrderItem oItem : items) {
+			sum += oItem.getSubTotal();
+		}
+		return sum;
 	}
 
 	@Override
