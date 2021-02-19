173cd01ac86a5cd792956860bd583893e46b7071
Luciano Oliveira <lcoliveiraa@gmail.com>
2021-02-19 05:40:26 -0300

    Payment, one-to-one association

--- src/main/java/com/demospring/course/config/TestConfig.java
+++ src/main/java/com/demospring/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.demospring.course.entities.Category;
 import com.demospring.course.entities.Order;
 import com.demospring.course.entities.OrderItem;
+import com.demospring.course.entities.Payment;
 import com.demospring.course.entities.Product;
 import com.demospring.course.entities.User;
 import com.demospring.course.entities.enums.OrderStatus;
@@ -80,5 +81,8 @@ public class TestConfig implements CommandLineRunner{
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
 		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		orderRepository.save(o1);
 	}
 }
